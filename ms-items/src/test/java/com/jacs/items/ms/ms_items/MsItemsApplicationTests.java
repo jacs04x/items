8783a60c100b4fc2f65209f1c441b1c16eb354aa
jacs04x <jonatanacm@ciencias.unam.mx>
2025-07-20 23:41:17 -0600

    configuracion inicial, se consume cliente feign

---
+++ ms-items/src/test/java/com/jacs/items/ms/ms_items/MsItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacs.items.ms.ms_items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
